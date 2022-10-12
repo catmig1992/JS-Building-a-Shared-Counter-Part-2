d4c5a54be8d75f48c99efb69af9c007b3b2583c0
Catherine Miguel <catmig1992@gmail.com>
2022-10-12 13:47:00 -0700

    activity completed - counters work

--- public/index.js
+++ public/index.js
@@ -1,26 +1,39 @@
-async function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+async function main() {
+  const countContainer = document.querySelector("#count-container");
+  const incrementButton = document.querySelector("#increment-button");
+  const decrementButton = document.querySelector("#decrement-button");
 
-    const response = await fetch('http://localhost:9001/counter');
+  const response = await fetch("http://localhost:9001/counter");
 
-    const result = await response.json();
-    
-    let countValue = result.value;
+  const result = await response.json();
 
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
-    }
+  let counterObject = result;
 
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
-    }
+  function increment() {
+    counterObject.value++;
+    countContainer.textContent = counterObject.value;
+    updateCounter();
+  }
 
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
-    countContainer.textContent = countValue;
+  function decrement() {
+    counterObject.value--;
+    countContainer.textContent = counterObject.value;
+    updateCounter();
+  }
+
+  async function updateCounter() {
+    //Refresh the page & See the same incremented/decremented count from before you refreshed
+    await fetch("http://localhost:9001/counter", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(counterObject),
+    });
+  }
+
+  incrementButton.addEventListener("click", increment); //Click the Increment button
+  decrementButton.addEventListener("click", decrement); //Click the Decrement button
+  countContainer.textContent = counterObject.value; //See the count increase on-screen
 }
-main()
+main();
